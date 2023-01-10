52abf52e5e19c3c7a8603c5889d7ebd907f5f704
zhuyiming666 <122241495+zhuyiming666@users.noreply.github.com>
2023-01-10 09:49:51 +0800

    pull commit

--- src/main/java/com/zymGit/GitTest.java
+++ src/main/java/com/zymGit/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!");
-        System.out.println("push");
+        System.out.println("push test");
+        System.out.println("pull test");
     }
 }
